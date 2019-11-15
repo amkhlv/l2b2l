a63ce113e53aecd0a1ca305b28ca44aa526b960c
Andrei Mikhailov <amkhlv@gmail.com>
2019-11-15 20:07:21 -0300

    level limiting, sections inside slides

--- app/BystroToc.hs
+++ app/BystroToc.hs
@@ -1,6 +1,7 @@
 module BystroToc where
 
 import qualified Options.Applicative as O
+import           Control.Monad (when)
 import           Data.Semigroup ((<>))
 import           Text.HTML.TagSoup
 import           Text.HTML.TagSoup.Match
@@ -13,6 +14,7 @@ import           System.Console.ANSI
 data Clops = Clops
   { listHTMLs :: Bool
    , name :: Maybe String
+   , lim :: Int
   }
 
 data Section = Section 
@@ -25,8 +27,10 @@ scribbleName :: O.Parser (Maybe String)
 scribbleName = O.optional $ O.strOption (O.short 's' <> O.metavar "NAME")
 listHTMLsFlag :: O.Parser Bool
 listHTMLsFlag = O.switch (O.long "htmls" <> O.help "list HTMLs" )
+levelLimit :: O.Parser Int
+levelLimit = O.option O.auto (O.short 'L' <> O.value 10 <> O.metavar "LEVEL_LIMIT")
 parser :: O.Parser Clops
-parser = Clops <$> listHTMLsFlag <*> scribbleName
+parser = Clops <$> listHTMLsFlag <*> scribbleName <*> levelLimit
 opts :: O.ParserInfo Clops
 opts = O.info ( parser O.<**> O.helper) (O.fullDesc <> O.progDesc "print table of contents")
 
@@ -37,8 +41,8 @@ printHTMLs name = do
       toctags = filter (tagOpenAttrNameLit "a" "class" (== "toptoclink")) tags
   sequence_ [putStrLn $ name ++ "/" ++ fromAttrib "href" t | t <- toctags ]
 
-printSection :: Section -> IO ()
-printSection sec = do
+printSection :: Section -> Int -> IO ()
+printSection sec lim = when (level sec < lim) $ do
   setSGR [SetColor Foreground Dull Green]
   putStr $ concat (replicate (1 + level sec) "--")
   putStr " "
@@ -60,6 +64,8 @@ procElem' acc (Str x : xs) = procElem' (acc ++ x) xs
 procElem' acc (x:xs) = procElem' (acc ++ "❄") xs
 procElem :: [SExp] -> String
 procElem = procElem' []
+slide2TOC' :: [Section] -> [Either String SExp] -> [Section]
+slide2TOC' acc [] = acc
 scrbl2TOC' :: [Section] -> [Either String SExp] -> [Section]
 scrbl2TOC' acc [] = reverse acc
 scrbl2TOC' acc (Right (SExp (Sym "page": Str ttl: Keyword "tag": Str lbl: _)) : xs) = 
@@ -70,6 +76,14 @@ scrbl2TOC' acc (Right (SExp (Sym "subpage": Int n: Str ttl: Keyword "tag": Str l
   scrbl2TOC' (Section (fromInteger n) ttl Nothing:acc) xs 
 scrbl2TOC' acc (Right (SExp (Sym "subpage": Int n: SExp (Sym "elem" : ys) : Keyword "tag": Str lbl: _)) : xs) = 
   scrbl2TOC' (Section (fromInteger n) (procElem ys) Nothing:acc) xs 
+scrbl2TOC' acc (Right (SExp (Sym "slide": Str ttl: cs)) : xs) = 
+   scrbl2TOC' (scrbl2TOC' [] (Right <$> cs) ++ Section 0 ttl Nothing:acc) xs
+scrbl2TOC' acc (Right (SExp (Sym "section": cs)) : xs) = 
+  scrbl2TOC' (Section 1 (case head cs of {Str ttl -> ttl ;  SExp (Sym "elem" : ys) -> procElem ys}) Nothing: acc) xs
+scrbl2TOC' acc (Right (SExp (Sym "subsection": cs)) : xs) = 
+  scrbl2TOC' (Section 2 (case head cs of {Str ttl -> ttl ;  SExp (Sym "elem" : ys) -> procElem ys}) Nothing: acc) xs
+scrbl2TOC' acc (Right (SExp (Sym "subsubsection": cs)) : xs) = 
+  scrbl2TOC' (Section 3 (case head cs of {Str ttl -> ttl ;  SExp (Sym "elem" : ys) -> procElem ys}) Nothing: acc) xs
 scrbl2TOC' acc (x:xs) = scrbl2TOC' acc xs
 scrbl2TOC :: [Either String SExp] -> [Section]
 scrbl2TOC = scrbl2TOC' [] 
@@ -80,23 +94,23 @@ printName nm = do
   putStrLn nm
   setSGR [Reset]
 
-printTOC :: String -> IO ()
-printTOC name = do
+printTOC :: String -> Int -> IO ()
+printTOC name lim = do
   scrbl <- readFile $ name ++ ".scrbl"
   case parse scrblParser "" scrbl of
     Left _ -> putStrLn "ERROR"
-    Right x -> sequence_ [printSection sec | sec <- scrbl2TOC x]
+    Right x -> sequence_ [printSection sec lim | sec <- scrbl2TOC x]
 
 main = do
   options <- O.execParser opts
   case name options of
-    Just nm -> if listHTMLs options then printHTMLs nm else printTOC nm
+    Just nm -> if listHTMLs options then printHTMLs nm else printTOC nm (lim options)
     Nothing -> do
       confs <- BConf.getScribblingConfs
       sequence_ [
         do
           printName $ BConf.name x
-          printTOC $ BConf.name x
+          printTOC ( BConf.name x ) (lim options)
         | x <- confs
         ]
 
